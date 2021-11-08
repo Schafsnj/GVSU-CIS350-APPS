a3cffeeb375a41818f2c7b0c63b98595db239460
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-08 09:55:41 -0500

    Cant run

--- APPS_WEB_APP/APPS-Web_APP/Services/UsersDAO.cs
+++ APPS_WEB_APP/APPS-Web_APP/Services/UsersDAO.cs
@@ -10,7 +10,8 @@ namespace APPS_Web_APP.Services
     public class UsersDAO
     {
         string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=APPS-Project-Database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-                       User account = new User();
+        User account = new User();
+        
 
         public bool FindUserByNameAndPassword(User user)
         {
