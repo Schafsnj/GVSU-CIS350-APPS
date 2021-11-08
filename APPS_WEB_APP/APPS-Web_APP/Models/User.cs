b1eb8073e5be3b0a23078b8b55d66959e9f07d21
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-08 10:09:54 -0500

    Added validation on input

--- APPS_WEB_APP/APPS-Web_APP/Models/User.cs
+++ APPS_WEB_APP/APPS-Web_APP/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,15 +12,22 @@ namespace APPS_Web_APP.Models
         
         public int Id { get; set; }
 
+        [Required]
         [DisplayName("Username")]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
 
+        [Required]
         [DisplayName("First Name")]
         public string FirstName { get; set; }
 
+        [Required]
         [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [Required]
         public string Email { get; set; }
         public string Role { get; set; }
     }
