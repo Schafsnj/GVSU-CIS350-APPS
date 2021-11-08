d7e545d620867b632ee97793044f096b36ad79e0
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-08 09:32:08 -0500

    Start commit added role for user

--- APPS_WEB_APP/APPS-Web_APP/Models/User.cs
+++ APPS_WEB_APP/APPS-Web_APP/Models/User.cs
@@ -13,5 +13,6 @@ namespace APPS_Web_APP.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Role { get; set; }
     }
 }
