710dfc839d12b9c50e68aa2272bce555b8c3fa27
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-08 18:22:24 -0500

    .

---
+++ APPS_WEB_APP/APPS-Web_APP/Models/AddUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APPS_Web_APP.Models
+{
+    public class AddUser
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 4)]
+
+        [DisplayName("Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 9)]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        public int Role { get; set; }
+    }
+}
