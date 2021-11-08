14ca481b9a8ced6b14d0eb0fa3d679b04f9caef8
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-08 12:03:24 -0500

    .

---
+++ APPS_WEB_APP/APPS-Web_APP/Controllers/test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APPS_Web_APP.Controllers
+{
+    public class test : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
