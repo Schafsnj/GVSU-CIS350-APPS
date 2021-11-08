b7557bca1b1c9cd75914d3771c5c7f08e7ac50b2
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-08 10:06:40 -0500

    .

--- APPS_WEB_APP/APPS-Web_APP/Models/User.cs
+++ APPS_WEB_APP/APPS-Web_APP/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,17 @@ namespace APPS_Web_APP.Models
 {
     public class User
     {
+        
         public int Id { get; set; }
+
+        [DisplayName("Username")]
         public string UserName { get; set; }
         public string Password { get; set; }
+
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
