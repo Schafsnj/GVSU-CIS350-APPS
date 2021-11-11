56f9ddf8796acf07cc9e2d830b733e65acae97df
Jayden Schafsnitz <schafsnj@mail.gvsu.edu>
2021-11-11 13:35:06 -0500

    .

--- APPS_WEB_APP/APPS-Web_APP/Services/UsersDAO.cs
+++ APPS_WEB_APP/APPS-Web_APP/Services/UsersDAO.cs
@@ -94,9 +94,10 @@ namespace APPS_Web_APP.Services
                     {
   
                         success = Crypto.VerifyHashedPassword(passwords[i], user.Password);
-                    user.Password = passwords[i];
+         
                     }
-                }
+                user.Password = passwords[i];
+            }
                   
                 return success;
         }
